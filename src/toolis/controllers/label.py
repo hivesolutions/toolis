54d4b37b4732d8451efb9545984355a75061322c
joamag <joamag@gmail.com>
2017-07-17 18:05:55 +0100

    new label printing

--- src/toolis/controllers/label.py
+++ src/toolis/controllers/label.py
@@ -25,3 +25,38 @@ class LabelController(appier.Controller):
             etag = image.etag,
             cache = True
         )
+
+    @appier.route("/labels/<int:id>/label.small", "GET")
+    def small_label(self):
+        label = toolis.Label.get(
+            id = id,
+            fields = ("image",),
+            rules = False
+        )
+        contents = self.template(
+            "label.small.html.tpl",
+            label = label
+        )
+
+        #@todo better structure this code
+        contents_e = contents.encode("utf-8")
+        input = appier.legacy.BytesIO(contents_e)
+        output = appier.legacy.BytesIO()
+        try:
+            def link_callback(uri, rel):
+                path = os.path.join(app.path, "static", uri)
+                return path
+
+            pisa_status = xhtml2pdf.pisa.CreatePDF(
+                contents,
+                dest = output,
+                link_callback = link_callback
+            )
+            data = output.getvalue()
+        finally:
+            output.close()
+
+        return self.send_file(
+            contents,
+            content_type = "application/pdf"
+        )
